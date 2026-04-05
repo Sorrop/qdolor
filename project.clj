bce04afc00389dcccab6647d3e45216edb916d45
Chris Klaoudianis <chris.klaou@gmail.com>
2026-04-05 18:18:37 +0300

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.sorrop/qdolor "0.1.1"
+(defproject net.clojars.sorrop/qdolor "0.1.2-SNAPSHOT"
   :description "Toolset for queue based execution by concurrent workers"
   :url "https://github.com/Sorrop/qdolor"
 
